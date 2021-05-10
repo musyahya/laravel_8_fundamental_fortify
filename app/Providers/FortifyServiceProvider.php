352c0075115d43bddbd9549aa712413e291c887d
musyahya <mananabdul37654@gmail.com>
2021-05-10 16:46:52 +0700

    register

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -43,5 +43,10 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        // register
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
     }
 }
