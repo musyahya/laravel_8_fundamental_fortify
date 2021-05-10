7a484578f315f7da74a90c15fca0e09ae97ef644
musyahya <mananabdul37654@gmail.com>
2021-05-10 20:25:04 +0700

    login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -48,5 +48,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+
+        // login
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
     }
 }
